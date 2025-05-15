830688ce447c13cbf4f2338a92f486d641a1cfc0
Lorena <Lorena@DESKTOP-BMMQISN>
2025-05-15 18:01:49 +0200

    Primer commit

---
+++ src/pruebagit/ClaseA.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebagit;
+
+/**
+ *
+ * @author Lorena
+ */
+public class ClaseA {
+    private final int NUM_ALUMNOS = 25;
+    private String nombre_profe;
+
+    public ClaseA() {
+    }
+
+    public ClaseA(String nombre_profe) {
+        this.nombre_profe = nombre_profe;
+    }
+
+    public String getNombre_profe() {
+        return nombre_profe;
+    }
+
+    public void setNombre_profe(String nombre_profe) {
+        this.nombre_profe = nombre_profe;
+    }
+
+    public int getNUM_ALUMNOS() {
+        return NUM_ALUMNOS;
+    }
+    
+    
+    
+}
